b29b68ae1cb401813add0e767d00059ff0a472d1
Prakash Jangid <prakashjangid7357@gmail.com>
2026-06-09 16:45:16 +0530

    feat: new version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.get("/", (req, res)=>{
-    return res.json({message: "Server is healthy"})
+    return res.json({message: "Server is healthy v2"})
 })
 
 app.listen(PORT, ()=> console.log(`Server is running on PORT: ${PORT}`))
